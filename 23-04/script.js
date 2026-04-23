69fb7382836f9b8d197884f5c36b26073461e5a0
DwiPutra-76 <kavhyarachman123@gmail.com>
2026-04-23 12:14:50 +0700

    p

---
+++ 23-04/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $.ajax({
+    type: "get",
+    url: "https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json",
+    success: function (r) {
+      let data = '';
+      for (let i = 0; i < r.length; i++) {
+        data += <li>${r[i].name}</li>
+      }
+      $("#list").html(data);
+    },
+  });
+});
